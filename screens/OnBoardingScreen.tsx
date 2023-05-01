6bf8e5983283bdbe09bca67936a90db0b09748dc
afiatamanna06 <afiatamanna06@gmail.com>
2023-05-01 23:16:00 +0600

    Add onboarding font

--- screens/OnBoardingScreen.tsx
+++ screens/OnBoardingScreen.tsx
@@ -1,16 +1,62 @@
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import { useFonts } from "expo-font";
+import React, { useCallback } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import * as SplashScreen from 'expo-splash-screen';
 
+SplashScreen.preventAutoHideAsync();
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
 const OnBoardingScreen = () => {
+
+  let [fontsLoaded] = useFonts({
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={{ flex: 1, width: width, height: height }}>
+    <View style={{ flex: 1, width: width, height: height }} onLayout={onLayoutRootView}>
       <Image
         source={require("../assets/images/onboarding.jpeg")}
         style={{ width: width, height: height, resizeMode: "cover", flex: 1 }}
       />
+      <View style={{ position: "absolute", bottom: 0 }}>
+        <LinearGradient
+          colors={[`rgba(0, 0, 0, 0)`, `#000`]}
+          style={{ position: "relative", width: width, height: height / 2.3 }}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              position: "relative",
+              textTransform: "capitalize",
+              fontSize: 32,
+              fontWeight: "600",
+              fontFamily: "Poppins-SemiBold",
+              textAlign: "center",
+            }}
+          >
+            Stay healthy even if you stay at home
+          </Text>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
