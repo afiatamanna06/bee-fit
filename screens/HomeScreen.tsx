9f81650a294c85a251f339e0a1420b7284c7a155
afiatamanna06 <afiatamanna06@gmail.com>
2023-05-02 19:26:22 +0600

    Add home screen navigation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+} from "react-native";
+import React, { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { AppParams } from "../components/navigation/FlowNavigation";
+import { useFonts } from "expo-font";
 
-const HomeScreen = () => {
+SplashScreen.preventAutoHideAsync();
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+type navigationPropTypes = StackNavigationProp<AppParams, "Home">;
+
+const HomeScreen: React.FC<navigationPropTypes> = ({ navigation }: any) => {
+  let [fontsLoaded] = useFonts({
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Image
+            source={require("../assets/images/avatar.jpeg")}
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+            }}
+          />
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                color: "#fff",
+                fontFamily: "Poppins-Regular",
+              }}
+            >
+              Welcome, user
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
