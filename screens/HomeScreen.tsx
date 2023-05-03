5f3ca4266aca026fdc7b2b5e804926533f65d179
afiatamanna06 <afiatamanna06@gmail.com>
2023-05-03 22:09:52 +0600

    Add home workouts section

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -20,6 +20,7 @@ import IconButton from "../components/common/IconButton";
 import HomeSearchBar from "../components/home/HomeSearchBar";
 import CategoryList from "../components/home/CategoryList";
 import SectionHeader from "../components/common/SectionHeader";
+import { workouts } from "../data";
 
 SplashScreen.preventAutoHideAsync();
 const width = Dimensions.get("window").width;
@@ -60,6 +61,81 @@ const HomeScreen: React.FC<navigationPropTypes> = ({ navigation }: any) => {
           <HomeSearchBar />
           <CategoryList />
           <SectionHeader title="Featured Workouts" />
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            decelerationRate='fast'
+            pagingEnabled
+            snapToInterval={290}
+            style={{ marginHorizontal: 15 }}
+          >
+            {workouts.map((workout) => (
+              <TouchableOpacity
+                key={workout.id}
+                style={{
+                  backgroundColor: "#1F212C",
+                  marginRight: 20,
+                  borderRadius: 8,
+                  overflow: "hidden",
+                }}
+              >
+                <Image
+                  source={workout.image}
+                  style={{
+                    width: 270,
+                    height: 200,
+                    borderTopLeftRadius: 8,
+                    borderTopRightRadius: 8,
+                  }}
+                />
+                <View style={{ padding: 10 }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      marginBottom: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        color: "#fff",
+                        fontFamily: "Poppins-SemiBold",
+                      }}
+                    >
+                      {workout.name}
+                    </Text>
+                    <View
+                      style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                      <Ionicons name="star" color={"#F2E205"} size={20} />
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          color: "#fff",
+                          fontFamily: "Poppins-Regular",
+                          marginTop: 3,
+                          marginLeft: 5,
+                        }}
+                      >
+                        {workout.rating}
+                      </Text>
+                    </View>
+                  </View>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fff",
+                      fontFamily: "Poppins-Regular",
+                    }}
+                  >
+                    {workout.coach}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         </View>
       </ScrollView>
     </SafeAreaView>
