e15d652088554a1fd78e2dc18a0ffc11c703322a
afiatamanna06 <afiatamanna06@gmail.com>
2023-05-18 11:04:20 +0600

    Add plan overview exercises mapping

--- screens/PlanOverviewScreen.tsx
+++ screens/PlanOverviewScreen.tsx
@@ -8,6 +8,7 @@ import {
   ScrollView,
   Dimensions,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import IconButton from "../components/common/IconButton";
 import { Ionicons } from "@expo/vector-icons";
@@ -260,10 +261,73 @@ const PlanOverviewScreen: React.FC<navigationPropTypes> = ({
               color: "#fff",
               fontFamily: "Poppins-SemiBold",
               marginTop: 10,
+              marginBottom: 8,
             }}
           >
             Exercises ({workout.exercises.length})
           </Text>
+          {workout.exercises.map((exercise: any) => (
+            <TouchableOpacity
+              key={exercise.id}
+              activeOpacity={0.7}
+              style={{
+                padding: 5,
+                marginBottom: 15,
+                backgroundColor: "#1F212C",
+                borderRadius: 8,
+                flexDirection: "row",
+              }}
+            >
+              <Image
+                source={exercise.image}
+                style={{
+                  width: 100,
+                  height: 100,
+                  borderRadius: 8,
+                  marginRight: 10,
+                }}
+              />
+              <View style={{ justifyContent: "space-between" }}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    color: "#fff",
+                    fontFamily: "Poppins-SemiBold",
+                  }}
+                >
+                  {exercise.name}
+                </Text>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Ionicons name="time-outline" color={"#fff"} size={16} />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fff",
+                      fontFamily: "Poppins-Regular",
+                      marginTop: 3,
+                      marginLeft: 5,
+                    }}
+                  >
+                    {exercise.time} / {exercise.set} set
+                  </Text>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Ionicons name="play" color={"#ADF547"} size={16} />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: "#fff",
+                      fontFamily: "Poppins-Regular",
+                      marginTop: 3,
+                      marginLeft: 5,
+                    }}
+                  >
+                    Play
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
