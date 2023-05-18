8878aedbe2dab527c31a0a565248a56eb9b64e91
afiatamanna06 <afiatamanna06@gmail.com>
2023-05-18 11:51:22 +0600

    Add plan overview workout exercises section

--- screens/PlanOverviewScreen.tsx
+++ screens/PlanOverviewScreen.tsx
@@ -17,6 +17,7 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { AppParams } from "../components/navigation/FlowNavigation";
 import { LinearGradient } from "expo-linear-gradient";
 import CommonButton from "../components/common/CommonButton";
+import WorkoutExercises from "../components/plan-overview/WorkoutExercises";
 
 SplashScreen.preventAutoHideAsync();
 const width = Dimensions.get("window").width;
@@ -258,86 +259,26 @@ const PlanOverviewScreen: React.FC<navigationPropTypes> = ({
           >
             {workout.description}
           </Text>
-          <Text
-            style={{
-              fontSize: 17,
-              color: "#fff",
-              fontFamily: "Poppins-SemiBold",
-              marginTop: 10,
-              marginBottom: 8,
-            }}
-          >
-            Exercises ({workout.exercises.length})
-          </Text>
-          {workout.exercises.map((exercise: any) => (
-            <TouchableOpacity
-              key={exercise.id}
-              activeOpacity={0.7}
-              style={{
-                padding: 5,
-                marginBottom: 15,
-                backgroundColor: "#1F212C",
-                borderRadius: 8,
-                flexDirection: "row",
-              }}
-            >
-              <Image
-                source={exercise.image}
-                style={{
-                  width: 100,
-                  height: 100,
-                  borderRadius: 8,
-                  marginRight: 10,
-                }}
-              />
-              <View style={{ justifyContent: "space-between" }}>
-                <Text
-                  style={{
-                    fontSize: 16,
-                    color: "#fff",
-                    fontFamily: "Poppins-SemiBold",
-                  }}
-                >
-                  {exercise.name}
-                </Text>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                  <Ionicons name="time-outline" color={"#fff"} size={16} />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      color: "#fff",
-                      fontFamily: "Poppins-Regular",
-                      marginTop: 3,
-                      marginLeft: 5,
-                    }}
-                  >
-                    {exercise.time} / {exercise.set} set
-                  </Text>
-                </View>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                  <Ionicons name="play" color={"#ADF547"} size={16} />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      color: "#fff",
-                      fontFamily: "Poppins-Regular",
-                      marginTop: 3,
-                      marginLeft: 5,
-                    }}
-                  >
-                    Play
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          ))}
+          <WorkoutExercises workout={workout} />
         </View>
       </ScrollView>
       <LinearGradient
-        style={{ position: "absolute", width: width, bottom: 0, paddingVertical: 15 }}
+        style={{
+          position: "absolute",
+          width: width,
+          bottom: 0,
+          paddingVertical: 15,
+        }}
         colors={["rgba(0, 0, 0, .1)", "rgba(0, 0, 0, 1)"]}
       >
-        <View style={{ width: width, display: "flex", alignItems:"center", justifyContent: "center", }}>
+        <View
+          style={{
+            width: width,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <CommonButton
             title="Start Workout"
             backgroundColor="#ADF547"
